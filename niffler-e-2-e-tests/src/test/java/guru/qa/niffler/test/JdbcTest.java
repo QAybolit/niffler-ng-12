2238916644b4b37439e11beca3206a25a00dc310
Dina <sd@myexe.net>
2026-06-21 00:16:28 +0300

    Create entities for jdbc

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/JdbcTest.java
@@ -0,0 +1,34 @@
+package guru.qa.niffler.test;
+
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
+import guru.qa.niffler.model.SpendJson;
+import guru.qa.niffler.service.SpendDbClient;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class JdbcTest {
+
+    @Test
+    public void daoTest() {
+        SpendDbClient spendDbClient = new SpendDbClient();
+        SpendJson spendJson = spendDbClient.createSpend(
+                new SpendJson(
+                        null,
+                        new Date(),
+                        new CategoryJson(
+                                null,
+                                "test-name",
+                                "Dina",
+                                false
+                        ),
+                        CurrencyValues.RUB,
+                        1000.0,
+                        "test description",
+                        "Dina"
+                )
+        );
+        System.out.println("JSON ===> " +  spendJson);
+    }
+}
